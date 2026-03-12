aed5aab41fd66dcd22a6b11ded21feae3a877ab0
Nigel Powell <nigel@thesadsongco.com>
2026-03-12 09:15:46 +0000

    Shop styling tweaks

--- controllers/shop/item.php
+++ controllers/shop/item.php
@@ -17,6 +17,8 @@ $item = $db->query($query, [$paths[2]])->fetch();
 if ($item['image'] == "") unset($item['image']);
 else $item['image_path'] = "/serve/" . SHOP_ASSET_PATH . "images/" . str_replace(".", "/", $item['image']);
 
+$item['description'] = nl2br($item['description']);
+
 $item_options = $db->query("SELECT * FROM Item_options WHERE item_id = ? AND Item_options.option_stock > 0", [$paths[2]])->fetchAll();
 
 $item['option'] = sizeof($item_options) > 0 ? ['options'=>$item_options] : false;
