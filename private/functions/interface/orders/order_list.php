21a3520ff6b76a40642ce5b9e7fdfa54180b87e9
Nigel Powell <nigel@thesadsongco.com>
2026-04-06 11:54:04 +0100

    All orders to be shipped filter on orders admin page

--- private/functions/interface/orders/order_list.php
+++ private/functions/interface/orders/order_list.php
@@ -21,6 +21,12 @@ switch ($filter) {
     case 'dispatched':
         $filter_text = "WHERE New_Orders.rm_tracking_number IS NOT NULL";
         break;
+    case 'new_all':
+        $filter_text = "WHERE New_Orders.rm_order_identifier IS NULL
+        AND New_Orders.transaction_id IS NOT NULL
+        AND New_Orders.shipping_method != 1
+        AND New_Orders.shipping_method != 7";
+        break;
     case 'new_uk':
         $filter_text = "WHERE New_Orders.rm_order_identifier IS NULL AND New_Orders.transaction_id IS NOT NULL";
         $area_filter = "AND Customers.country = 31";
